ef4dcd633c32eaae9e8cabeefef47c6f7a0ad34b
Henare Degan <henare.degan@gmail.com>
2015-09-16 16:07:02 +1000

    Fetch vote data from the morph.io API

--- server.rb
+++ server.rb
@@ -1,6 +1,17 @@
 require "sinatra"
 require "sinatra/json"
+require "json"
+require "open-uri"
+
+def morph_scraper_query(scraper_name, query)
+  puts "Querying morph.io scraper, #{scraper_name}, for: #{query}"
+  url = "https://api.morph.io/#{scraper_name}/data.json?key=#{CGI.escape(ENV['MORPH_API_KEY'])}&query=#{CGI.escape(query)}"
+  JSON.parse(open(url).read)
+end
 
 get "/vote_event/:id" do |id|
-  json foo: id
+  vote_event = morph_scraper_query(ENV["MORPH_SCRAPER_NAME"], "SELECT * FROM 'vote_events' WHERE identifier='#{id}'").first
+  votes = morph_scraper_query(ENV["MORPH_SCRAPER_NAME"], "SELECT * FROM 'votes' WHERE vote_event_id='#{id}'")
+
+  json vote_event.merge(votes: votes)
 end
