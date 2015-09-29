2ea735f75aa71f880477c7bb73a39ae1f22a4358
Henare Degan <henare.degan@gmail.com>
2015-09-29 14:54:06 +1000

    Also include bill data from scraper

--- server.rb
+++ server.rb
@@ -14,18 +14,21 @@ end
 get "/vote_event/:id" do |id|
   vote_event = morph_scraper_query(ENV["MORPH_SCRAPER_NAME"], "SELECT * FROM 'vote_events' WHERE identifier='#{id}'").first
   votes = morph_scraper_query(ENV["MORPH_SCRAPER_NAME"], "SELECT * FROM 'votes' WHERE vote_event_id='#{id}'")
+  bills = morph_scraper_query(ENV["MORPH_SCRAPER_NAME"], "SELECT * FROM 'bills' WHERE vote_event_id='#{id}'")
 
-  json vote_events: [vote_event.merge(votes: votes)]
+  json vote_events: [vote_event.merge(votes: votes, bills: bills)]
 end
 
 get "/vote_events/:date" do |date|
   vote_events = morph_scraper_query(ENV["MORPH_SCRAPER_NAME"], "SELECT * FROM 'vote_events' WHERE date(start_date)='#{date}'")
   vote_event_ids = vote_events.map { |ve| "'#{ve["identifier"]}'" }.join(", ")
+  all_bills = morph_scraper_query(ENV["MORPH_SCRAPER_NAME"], "SELECT * FROM 'bills' WHERE vote_event_id IN (#{vote_event_ids})")
   all_votes = morph_scraper_query(ENV["MORPH_SCRAPER_NAME"], "SELECT * FROM 'votes' WHERE vote_event_id IN (#{vote_event_ids})")
 
   vote_events.map! do |ve|
     votes = all_votes.select { |v| v["vote_event_id"] == ve["identifier"] }
-    ve.merge(votes: votes)
+    bills = all_bills.select { |v| v["vote_event_id"] == ve["identifier"] }
+    ve.merge(votes: votes, bills: bills)
   end
 
   json vote_events: vote_events
