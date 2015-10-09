49dbe295850e18312f757bdcd530891a364f4f91
Henare Degan <henare.degan@gmail.com>
2015-10-09 12:14:39 +1100

    Nest source URLs under the Popolo sources array. Fixes #2

--- server.rb
+++ server.rb
@@ -11,8 +11,18 @@ def morph_scraper_query(scraper_name, query)
   JSON.parse(open(url).read)
 end
 
+# Nests source_url and debate_url under the Popolo sources array
+def nest_sources(h)
+  h[:sources] = [
+    {source_url: h.delete("source_url"), note: "Source URL"},
+    {debate_url: h.delete("debate_url"), note: "Debate URL"}
+  ]
+
+  h
+end
+
 get "/vote_event/:id" do |id|
-  vote_event = morph_scraper_query(ENV["MORPH_SCRAPER_NAME"], "SELECT * FROM 'vote_events' WHERE identifier='#{id}'").first
+  vote_event = nest_sources(morph_scraper_query(ENV["MORPH_SCRAPER_NAME"], "SELECT * FROM 'vote_events' WHERE identifier='#{id}'").first)
   votes = morph_scraper_query(ENV["MORPH_SCRAPER_NAME"], "SELECT * FROM 'votes' WHERE vote_event_id='#{id}'")
   bills = morph_scraper_query(ENV["MORPH_SCRAPER_NAME"], "SELECT * FROM 'bills' WHERE vote_event_id='#{id}'")
 
@@ -28,7 +38,7 @@ get "/vote_events/:date" do |date|
   vote_events.map! do |ve|
     votes = all_votes.select { |v| v["vote_event_id"] == ve["identifier"] }
     bills = all_bills.select { |v| v["vote_event_id"] == ve["identifier"] }
-    ve.merge(votes: votes, bills: bills)
+    nest_sources(ve).merge(votes: votes, bills: bills)
   end
 
   json vote_events: vote_events
